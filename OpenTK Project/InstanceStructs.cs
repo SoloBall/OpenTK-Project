d89819bf56b996118ec351029139ba029adb7ecd
soloball <arma0130@edu.sde.dk>
2026-01-27 13:15:51 +0100

    Added padding to rectangle struct

--- OpenTK Project/InstanceStructs.cs
+++ OpenTK Project/InstanceStructs.cs
@@ -6,13 +6,22 @@ namespace OpenTK_Project
     public class InstanceStructs
     {
         [StructLayout(LayoutKind.Sequential)]
-        public struct RectangleInstance
+        public unsafe struct RectangleInstance
         {
-            public int Length, Height, Width;
+            public float Length;
+            public float Height;
+            public float Width;
+            private float _pad0; // padding for vec3 alignment
+
             public Vector3 Position;
+            private float _pad1; // vec3 -> 16 bytes
+
             public Color4 Color;
+
             public float Selected;
+            private Vector3 _pad2; // struct size multiple of 16
         }
+
     }
 
 }
